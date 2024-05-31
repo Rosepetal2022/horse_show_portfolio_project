754d69e54825b00145431a07d6d1e8ad68df9f02
DusterFan668 <106773491+chasec22@users.noreply.github.com>
2024-05-30 20:07:29 -0700

    Horse show CRUD

--- App/frontend/src/pages/HorseShows.jsx
+++ App/frontend/src/pages/HorseShows.jsx
@@ -9,7 +9,7 @@ function HorseShows() {
         const [horseShowsData, setHorseShowData] = useState([]);
   
         // Define a function to fetch diagnostic data from the API
-        const fetchHorseData = async () => {
+        const fetchHorseShowData = async () => {
           try {
             // Construct the URL for the API call
             const URL = import.meta.env.VITE_API_URL + 'horseShows';
@@ -28,8 +28,77 @@ function HorseShows() {
       
         // useEffect hook to trigger the fetchDiagnosticData function when the component mounts
         useEffect(() => {
-          fetchHorseData();
+          fetchHorseShowData();
         }, []);
+
+    const [HorseShowID, setHorseShowID] = useState('')
+    const [selectedHorseShow, setSelectedHorseShow] = useState(null);
+    const [HorseShowName, setHorseShowName] = useState('')
+    const [ShowDate, setShowDate] = useState('')
+    const [Location, setLocation] = useState('')
+    const [PrizeMoneyOffered, setPrizeMoneyOffered] = useState('')
+    const [NumEnteredHorse, setNumEnteredHorse] = useState('')
+
+    function horseShowSubmit(event){
+        event.preventDefault();
+         axios.post( import.meta.env.VITE_API_URL + 'horseShows', {HorseShowName, ShowDate, Location, PrizeMoneyOffered, NumEnteredHorse})
+        .then(res => {
+            console.log(res)
+            window.location.reload()
+        }).catch(err => console.log(err))
+    }
+
+    function horseShowUpdate(event, horseShowId) {
+        event.preventDefault();
+        console.log(horseShowId)
+
+        
+        // Gather form data
+        const horseShowName = event.target.elements.horseShowName.value;
+        const showDate = event.target.elements.showDate.value;
+        const location = event.target.elements.location.value;
+        const prizeMoneyOffered = event.target.elements.prizeMoneyOffered.value;
+        const numEnteredHorse = event.target.elements.numEnteredHorse.value;
+    
+        // Make PUT request to update horseShow
+        axios.put( import.meta.env.VITE_API_URL + `horseShows/${horseShowId}`, { HorseShowID: horseShowId, HorseShowName: horseShowName, ShowDate: showDate, Location: location, PrizeMoneyOffered: prizeMoneyOffered, NumEnteredHorse: numEnteredHorse })
+            .then(res => {
+                console.log(res);
+                // Optionally, handle success (e.g., display a success message)
+            })
+            .catch(err => {
+                console.error('Error updating horseShow:', err.response.data);
+                // Optionally, handle error (e.g., display an error message)
+            });
+    }
+
+    const horseShowDelete = async (HorseShowID) => {
+        console.log(HorseShowID)
+        try {
+        const URL =  import.meta.env.VITE_API_URL + 'horseShows/' + HorseShowID;
+        console.log(URL)
+        const response = await axios.delete(URL);
+          
+          // Handle success
+        if (response.status === 204) {
+            alert("HorseShow deleted successfully");
+            window.location.reload()
+        }
+        } catch (error) {
+          console.error('Error deleting horseShow:', error);
+          // Handle error
+        }
+      };
+
+      const handleHorseShowSelect = (horseShowId) => {
+        const selected = horseShowsData.find((horseShow) => horseShow.HorseShowID === parseInt(horseShowId));
+        setSelectedHorseShow(selected);
+        setHorseShowName(selected.HorseShowName);
+        setShowDate(selected.ShowDate);
+        setLocation(selected.Location);
+        setPrizeMoneyOffered(selected.PrizeMoneyOffered);
+        setNumEnteredHorse(selected.NumEnteredHorse)
+    };
 
     return (
         <>
@@ -44,7 +113,6 @@ function HorseShows() {
                     <th scope="col">Location</th>
                     <th scope="col">Prize Money Offered</th>
                     <th scope="col"># of Horses Entered</th>
-                    <th scope="col">Edit</th>
                     <th scope="col">Delete</th>
                 </tr>
             </thead>
@@ -57,8 +125,7 @@ function HorseShows() {
                         <td>{horseShow.Location}</td>
                         <td>{horseShow.PrizeMoneyOffered}</td>
                         <td>{horseShow.NumEnteredHorse}</td>
-                        <td><button className="btn btn-primary btn-sm" ><FaEdit /></button></td>
-                        <td><button className="btn btn-danger btn-sm ml-1"><FaDeleteLeft /></button></td>
+                        <td><button className="btn btn-danger btn-sm ml-1" onClick={e => horseShowDelete(horseShow.HorseShowID)}><FaDeleteLeft /></button></td>
                     </tr>
                 ))}
             </tbody>
@@ -68,31 +135,98 @@ function HorseShows() {
       <div className="form-size">
       <div className="container form-background">
       <h2>Add Horse Show</h2>
-      <form>
+      <form onSubmit={horseShowSubmit}>
         <div className="form-group form-padding">
           <label htmlFor="name">Name</label>
-          <input type="text" className="form-control" id="name" defaultValue="Name" />
+          <input type="text" className="form-control" id="name" defaultValue="Name" 
+          onChange={e => setHorseShowName(e.target.value)}
+          />
         </div>
         <div className="form-group form-padding">
           <label htmlFor="date">Date</label>
-          <input type="date" className="form-control" id="date" />
+          <input type="date" className="form-control" id="date" 
+          onChange={e => setShowDate(e.target.value)}
+          />
         </div>
         <div className="form-group form-padding">
           <label htmlFor="discipline">Location</label>
-          <input type="text" className="form-control" id="discipline" defaultValue="City, State" />
+          <input type="text" className="form-control" id="discipline" defaultValue="City, State" 
+          onChange={e => setLocation(e.target.value)}
+          />
         </div>
         <div className="form-group form-padding">
           <label htmlFor="prizeMoney">Prize Money Offered</label>
-          <input type="text" className="form-control" id="prizeMoney" defaultValue="Prize Money Offered" />
+          <input type="number" className="form-control" id="prizeMoney" defaultValue="Prize Money Offered" 
+          onChange={e => setPrizeMoneyOffered(e.target.value)}
+          />
         </div>
         <div className="form-group form-padding">
           <label htmlFor="prizeMoney">Number of Horses</label>
-          <input type="number" className="form-control" id="prizeMoney" defaultValue="0" />
+          <input type="number" className="form-control" id="prizeMoney" defaultValue="0" 
+          onChange={e => setNumEnteredHorse(e.target.value)}
+          />
         </div>
         <button type="submit" className="btn btn-primary">Add</button>
       </form>
     </div>
     </div>
+    <div className="form-size">
+        <div className="form-background container">
+          <div className="form-group form-padding">
+                      <label htmlFor="horseShowSelect">Select HorseShow</label>
+                      <select className="form-control" id="horseShowSelect" onChange={(e) => handleHorseShowSelect(e.target.value)}>
+                          <option value="">Select a Horse Show</option>
+                            {horseShowsData.map((horseShow) => (
+                                <option key={horseShow.HorseShowID} value={horseShow.HorseShowID}>
+                                    {`${horseShow.HorseShowName}`}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                {selectedHorseShow && (
+                       <form onSubmit={(e) => horseShowUpdate(e, selectedHorseShow.HorseShowID)}>
+                       {/* Hidden input field to store HorseShowID */}
+                       <input type="hidden" value={selectedHorseShow.HorseShowID} />
+                       <div className="form-group form-padding">
+                    <label htmlFor="horseShowName">Name</label>
+                    <input type="text" className="form-control" id="horseShowName" defaultValue="Name" 
+                    value={HorseShowName}
+                    onChange={e => setHorseShowName(e.target.value)}
+                    />
+                </div>
+                <div className="form-group form-padding">
+                  <label htmlFor="showDate">Date</label>
+                  <input type="date" className="form-control" id="showDate" 
+                  value={ShowDate}
+                  onChange={e => setShowDate(e.target.value)}
+                  />
+                </div>
+                <div className="form-group form-padding">
+                  <label htmlFor="location">Location</label>
+                  <input type="text" className="form-control" id="location" defaultValue="City, State" 
+                  value={Location}
+                  onChange={e => setLocation(e.target.value)}
+                  />
+                </div>
+                <div className="form-group form-padding">
+                  <label htmlFor="prizeMoneyOffered">Prize Money Offered</label>
+                  <input type="number" className="form-control" id="prizeMoneyOffered" defaultValue="Prize Money Offered" 
+                  value={PrizeMoneyOffered}
+                  onChange={e => setPrizeMoneyOffered(e.target.value)}
+                  />
+                </div>
+                <div className="form-group form-padding">
+                  <label htmlFor="numEnteredHorse">Number of Horses</label>
+                  <input type="number" className="form-control" id="numEnteredHorse" defaultValue="0" 
+                  value={NumEnteredHorse}
+                  onChange={e => setNumEnteredHorse(e.target.value)}
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary">Add</button>
+                        </form>
+                    )}
+              </div>
+          </div>
     </div>
     </>
     );
