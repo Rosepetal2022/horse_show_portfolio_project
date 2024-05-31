6c0c919b7788dc02dbdba942735fa6f88353576a
DusterFan668 <106773491+chasec22@users.noreply.github.com>
2024-05-30 20:05:55 -0700

    Update horseShows.js

--- App/backend/routes/horseShows.js
+++ App/backend/routes/horseShows.js
@@ -2,9 +2,15 @@ const express = require("express");
 const router = express.Router();
 const {
   getHorseShows,
+  createHorseShow,
+  deleteHorseShow,
+  updateHorseShow
   
 } = require("../controllers/horseShowController");
 
 router.get("/", getHorseShows);
+router.post("/", createHorseShow);
+router.delete("/:HorseShowID", deleteHorseShow);
+router.put("/:id", updateHorseShow);
 
 module.exports = router;
