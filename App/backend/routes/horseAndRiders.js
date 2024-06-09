a9758f15c34473268a6023f9a2655c73e55e6edf
DusterFan668 <106773491+chasec22@users.noreply.github.com>
2024-06-09 13:29:45 -0700

    Update horseAndRiders.js

--- App/backend/routes/horseAndRiders.js
+++ App/backend/routes/horseAndRiders.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const {
-  getHorsesAndRiders,
-  
+  // getHorsesAndRiders,
+  getSpecificHorse,
+  deleteHorseAndRider,
+  createHAndR,
+  updateHAndR
 } = require("../controllers/horseAndRiderController");
 
-router.get("/", getHorsesAndRiders);
+// router.get("/", getHorsesAndRiders);
+router.get("/", getSpecificHorse);
+router.delete("/:HAndRID", deleteHorseAndRider);
+router.post("/", createHAndR)
+router.put("/:id", updateHAndR)
 
 module.exports = router;
